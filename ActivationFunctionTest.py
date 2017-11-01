788f33703d5255abb676b3b1083cf61230088b38
yyxyz <804683814@163.com>
2017-11-01 10:03:54 +0800

    新建文件

---
+++ ActivationFunctionTest.py
@@ -0,0 +1,58 @@
+## tensorflow 激励函数
+
+import tensorflow as tf
+import numpy as np
+
+## 定义层 layer
+def add_layer(inputs, in_size, out_size, activation_function=None):
+    Weights = tf.Variable(tf.random_normal([in_size, out_size]))
+    biases = tf.Variable(tf.zeros([1, out_size]) + 0.1)
+    Wx_plus_b = tf.matmul(inputs, Weights) + biases
+
+    if activation_function is None:
+        outputs = Wx_plus_b
+    else:
+        outputs = activation_function(Wx_plus_b)
+    
+    return outputs
+
+## 定义数据
+x_data = np.linesep(-1, 1, 300)[: np.newaxis]
+noise = np.random.normal(0, 0.05, x_data.shape)
+y_data = np.sequare(x_data) - 0.5 + noise
+
+xs = tf.placeholder(tf.float32,[None, 1])
+ys = tf.placeholder(tf.float32,[None, 1])
+
+## 定义神经层
+l1 = add_layer(xs, 1, 10, activation_function = tf.nn.relu)
+prediction = add_layer(l1, 10, 1, activation_function = None)
+
+## 计算损失函数
+loss = tf.reduce_mean(
+            tf.reduce_sum(
+                tf.sequare(ys - prediction), reduction_indices = [1]))
+
+## 开始训练
+train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
+
+init = tf.initialize_all_variables()
+sess = tf.Session()
+sess.run(init)
+
+for i in range(1000):
+    sess.run(train_step, feed_dict={xs:x_data, ys:y_data})
+
+    if i % 50:
+        print(sess.run(loss, feed_dict={xs:x_data, ys:y_data}))
+    
+
+
+
+
+
+
+
+
+
+
