ddad2fa42688de7aa0c28a787c208736bc67a7e7
yyxyz <804683814@163.com>
2017-11-01 15:55:33 +0800

    修改代码

--- ActivationFunctionTest.py
+++ ActivationFunctionTest.py
@@ -1,7 +1,10 @@
 ## tensorflow 激励函数
-
 import tensorflow as tf
 import numpy as np
+import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
 
 ## 定义层 layer
 def add_layer(inputs, in_size, out_size, activation_function=None):
@@ -13,25 +16,23 @@ def add_layer(inputs, in_size, out_size, activation_function=None):
         outputs = Wx_plus_b
     else:
         outputs = activation_function(Wx_plus_b)
-    
     return outputs
 
-## 定义数据
-x_data = np.linesep(-1, 1, 300)[: np.newaxis]
-noise = np.random.normal(0, 0.05, x_data.shape)
-y_data = np.sequare(x_data) - 0.5 + noise
 
-xs = tf.placeholder(tf.float32,[None, 1])
-ys = tf.placeholder(tf.float32,[None, 1])
+## 定义数据
+x_data = np.linspace(-1, 1, 3000)[:, np.newaxis]
+noise = np.random.normal(0, 0.05, x_data.shape)
+y_data = np.square(x_data) - 0.5 + noise
+
+xs = tf.placeholder(tf.float32, [None, 1])
+ys = tf.placeholder(tf.float32, [None, 1])
 
 ## 定义神经层
-l1 = add_layer(xs, 1, 10, activation_function = tf.nn.relu)
-prediction = add_layer(l1, 10, 1, activation_function = None)
+l1 = add_layer(xs, 1, 10, activation_function=tf.nn.relu)
+prediction = add_layer(l1, 10, 1, activation_function=None)
 
 ## 计算损失函数
-loss = tf.reduce_mean(
-            tf.reduce_sum(
-                tf.sequare(ys - prediction), reduction_indices = [1]))
+loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction), reduction_indices=[1]))
 
 ## 开始训练
 train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
@@ -41,18 +42,6 @@ sess = tf.Session()
 sess.run(init)
 
 for i in range(1000):
-    sess.run(train_step, feed_dict={xs:x_data, ys:y_data})
-
-    if i % 50:
-        print(sess.run(loss, feed_dict={xs:x_data, ys:y_data}))
-    
-
-
-
-
-
-
-
-
-
-
+    sess.run(train_step, feed_dict={xs: x_data, ys: y_data})
+    if i % 50 == 0:
+        print(sess.run(loss, feed_dict={xs: x_data, ys: y_data}))
